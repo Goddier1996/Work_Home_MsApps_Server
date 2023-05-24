ed3cd4348b2333aadcdfa9905937a171fc05d794
artium_96 <artium20@gmail.com>
2023-05-24 18:53:41 +0300

    fix bag

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require(`cors`)
 
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors())
 app.use(express.json())
@@ -13,6 +13,6 @@ app.use('/api/infoCategory', require('./routes/infoCategory'))
 
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
 });
